7cd2e2f0e38593a4ddec473d02377fb37cf2cf11
Peter Chang <peter.yaochen.chang@gmail.com>
2023-12-20 02:25:04 +0100

    modify Day06

--- Day06/solution1.py
+++ Day06/solution1.py
@@ -4,19 +4,11 @@
 # value will also make the product exceed the target without the need for actual
 # product calculations.
 
-def find_difference_to_exceed_target(number, target):
+def find_product_to_exceed_target(number, target):
   for i in range(1, number):
     if i * (number - i) > target:
       return (number - i) - i + 1
   return None
-
-def product_of_differences_in_range(start, end, target):
-  product = 1
-  for number in range(start, end + 1):
-      difference = find_difference_to_exceed_target(number, target)
-      if difference is not None:
-          product *= difference
-  return product
 
 # Part 1
 data = "[49, 356], [87, 1378], [78, 1502], [95, 1882]"
@@ -26,11 +18,11 @@ product = 1
 for pair in pairs:
   time = int(pair[0])
   distance = int(pair[1])
-  difference = find_difference_to_exceed_target(time, distance)
+  difference = find_product_to_exceed_target(time, distance)
   product *= difference
 
 print(f"The product of differences is {product}")
 
 # Part2
-difference = find_difference_to_exceed_target(49877895, 356137815021882)
+difference = find_product_to_exceed_target(49877895, 356137815021882)
 print(f"The result is {difference}")
